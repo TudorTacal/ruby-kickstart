6815be9949fb5993d9af47d30eba03f74a5c6b76
TudorTacal <tudortacal@gmail.com>
2016-10-18 11:34:16 +0000

    Completed Session 1:7, 7_string.rb

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -5,4 +5,14 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  new_string = String.new
+  string.chars.each_with_index do |x,i|
+    while i < string.length-1
+      new_string += string[i+1] if x=='r' || x=='R' 
+      break
+    end
+  end
+ new_string
 end
+
+
