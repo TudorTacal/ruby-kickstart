45b8b7b876677c5adfe3476b86824a000a2e2e8f
TudorTacal <tudortacal@gmail.com>
2016-10-18 21:37:47 +0000

    Completed Session 2:4, 4_array.rb

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,6 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+def get_squares(numbers)
+  numbers.each.select{|x| x if numbers.include?(x**2)}.sort
+end
