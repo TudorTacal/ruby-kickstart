17a19b812ce45e3f2946af58f654fdaabd9f030d
TudorTacal <tudortacal@gmail.com>
2016-10-18 21:10:34 +0000

    Completed Session 2:3, 3_array.rb

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -2,10 +2,11 @@
 # returns an array containing every other character
 #
 # example:
-# "abcdefg".every_other_char  # => "aceg"
+# "abcdef  g".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
 class String
   def every_other_char
+    self.chars.map.with_index{|x,i| x if i.even?}.join("")
   end
 end
