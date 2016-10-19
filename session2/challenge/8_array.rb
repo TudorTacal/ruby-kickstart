b1ef334387150778d11d119e8c5181d2c7ae81d4
TudorTacal <tudortacal@gmail.com>
2016-10-19 13:12:27 +0000

    Completed Session 2:8, 8_array

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,18 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(numbers)
+  i=0
+  test =[]
+  while i<numbers.size
+    if numbers[i]==numbers[i+1] && numbers[i]==numbers[i+2] 
+      test << i
+      return true
+    end
+    i+=1
+  end
+  return false if test.empty? 
+    
+end
+p got_three? [1,2, 2,3,3,5]
