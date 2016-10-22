ad30894ec5d759152b2ce0aa6ef7c36a101bb615
TudorTacal <tudortacal@gmail.com>
2016-10-22 21:43:23 +0000

    Completed Session 3:17, 17_hashes.rb

--- session3/challenge/17_hashes.rb
+++ session3/challenge/17_hashes.rb
@@ -7,3 +7,19 @@
 #
 # print_list_in_reverse head   # >> "1\n2\n"
 
+def print_list_in_reverse(list)
+  array = []
+  while list
+    array << list[:data]
+    list = list[:next]
+  end
+   puts array.reverse 
+end
+head = {:data => 1, :next => nil}
+head = {:data => 2, :next => head}
+head = {:data => 3, :next => head}
+head = {:data => 4, :next => head}
+head = {:data => 5, :next => head}
+head = {:data => 6, :next => head}
+
+print_list_in_reverse head
